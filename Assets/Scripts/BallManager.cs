13a9a3d13e84446e1110d949d2c64bdda811d7b7
auphan2019 <grozyeux@LAPTOP-J1E2287B.localdomain>
2021-02-19 16:27:49 +0100

    première balle

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 1)
+        {
+            gameObject.SetActive(true);
+        }
+        if (time > 2)
+        {
+            gameObject.SetActive(false);
+            time = 0;
+        }
+    }
+}

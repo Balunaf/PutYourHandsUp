2271d74a4bb4927cb7f0dd4d779bdba15b25f661
auphan2019 <grozyeux@LAPTOP-J1E2287B.localdomain>
2021-03-11 22:54:36 +0100

    mains essai

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -4,19 +4,11 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
-    [SerializeField] private Slider sliderMain;
-
     [SerializeField] private Text textMain;
-
-    [SerializeField] private Slider sliderDifficulte;
-
-    [SerializeField] private Text textDifficulte;
 
     public bool leftHand;
 
     public bool rightHand;
-
-    public int difficulte;
 
     public static LevelLoader instance;
     private void Awake()
@@ -33,44 +25,28 @@ public class LevelLoader : MonoBehaviour
     {
         leftHand = false;
         rightHand = false;
+        textMain.text = "Deux Mains";
     }
 
-    void Update()
+    public void ChangeHands()
     {
-        float main = sliderMain.value;
-        if(main == 1f)
+        if (!leftHand & !rightHand)
         {
-            textMain.text = "Deux mains";
-            rightHand = false;
             leftHand = false;
-        }
-        if(main == 2f)
-        {
-            textMain.text = "Main droite";
             rightHand = true;
-            leftHand = false;
+            textMain.text = "Main droite";
         }
-        if(main == 3f)
+        else if (!leftHand & rightHand)
         {
-            textMain.text = "Main gauche";
-            rightHand = false;
             leftHand = true;
+            rightHand = false;
+            textMain.text = "Main Gauche";
         }
-        float dif = sliderDifficulte.value;
-        if(dif == 1f)
+        else
         {
-            textDifficulte.text = "Difficulté facile";
-            difficulte = 1;
-        }
-        if(dif == 2f)
-        {
-            textDifficulte.text = "Difficulté moyenne";
-            difficulte = 2;
-        }
-        if(dif == 3f)
-        {
-            textDifficulte.text = "Difficulté difficile";
-            difficulte = 3;
+            leftHand = false;
+            rightHand = false;
+            textMain.text = "Deux Mains";
         }
     }
 

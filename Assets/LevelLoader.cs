a263d77fbeacab7bee4cacc58a6a9466a598ad5f
alan4998 <biglash4998@gmail.com>
2021-03-11 22:45:00 +0100

    nouveau menu

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
     public bool leftHand;
 
     public bool rightHand;
+
+    [SerializeField] Button mainBoutton;
 
     public static LevelLoader instance;
     private void Awake()
@@ -17,9 +20,34 @@ public class LevelLoader : MonoBehaviour
         }
         else instance = this;
     }
+    private void Start()
+    {
+        mainBoutton.onClick.AddListener(() => ChangeHands());
+        leftHand = false;
+        rightHand = false;
+    }
     public void LoadLevel(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ChangeHands()
+    {
+        if (!leftHand & !rightHand)
+        {
+            leftHand = false;
+            rightHand = true;
+        }
+        else if (!leftHand & rightHand)
+        {
+            leftHand = true;
+            rightHand = false;
+        }
+        else
+        {
+            leftHand = false;
+            rightHand = false;
+        }
     }
 
     public void QuitGame()

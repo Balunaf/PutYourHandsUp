c123abdeb01143a4090044f454f3cbbb07d2d4f7
auphan2019 <grozyeux@LAPTOP-J1E2287B.localdomain>
2021-03-12 10:57:15 +0100

    son menu

---
+++ Assets/Scripts/MenuAudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource sourceClickButton;
+
+    [SerializeField] private AudioClip clickButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ClickButton()
+    {
+        sourceClickButton.loop = false;
+        sourceClickButton.PlayOneShot(clickButton);
+    }
+}

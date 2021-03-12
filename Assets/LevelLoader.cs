f4d1131ebeb22f138a97922b2a6725cbcb93945c
alan4998 <biglash4998@gmail.com>
2021-03-12 09:43:55 +0100

    Update LevelLoader.cs

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI textMain;
+
     public bool leftHand;
 
     public bool rightHand;
 
     [SerializeField] Button mainBoutton;
+
+    public int difficulte;
 
     public static LevelLoader instance;
     private void Awake()
@@ -29,6 +34,8 @@ public class LevelLoader : MonoBehaviour
     public void LoadLevel(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+
+        textMain.text = "DEUX MAINS";
     }
 
     public void ChangeHands()
@@ -36,17 +43,23 @@ public class LevelLoader : MonoBehaviour
         if (!leftHand & !rightHand)
         {
             leftHand = false;
-            rightHand = true;
-        }
-        else if (!leftHand & rightHand)
-        {
-            leftHand = true;
             rightHand = false;
+            textMain.text = "MAIN DROITE";
         }
         else
         {
-            leftHand = false;
-            rightHand = false;
+            if (!leftHand & rightHand)
+            {
+                leftHand = true;
+                rightHand = false;
+                textMain.text = "MAIN GAUCHE";
+            }
+            else
+            {
+                leftHand = false;
+                rightHand = false;
+                textMain.text = "DEUX MAINS";
+            }
         }
     }
 

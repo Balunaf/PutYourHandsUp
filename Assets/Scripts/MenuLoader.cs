32833fbe7638cfd72fb4fe814a5253af636a6a90
auphan2019 <grozyeux@LAPTOP-J1E2287B.localdomain>
2021-03-14 19:18:02 +0100

    test

--- Assets/Scripts/MenuLoader.cs
+++ Assets/Scripts/MenuLoader.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI
 
 public class MenuLoader : MonoBehaviour
 {
+    [SerializeField] private Canvas canvas;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,10 @@ public class MenuLoader : MonoBehaviour
     public void LoadLevel(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    private void OnEnable()
+    {
+        canvas.gameObject.SetActive(false);
     }
 }
